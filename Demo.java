8b6630216f676ad9e9c1aa4bc8df0c48101bf20c
sudheep2 <sr811838@student.nitw.ac.in>
2022-01-18 11:54:09 +0530

    my demo commit

---
+++ Demo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mypackage;
+
+/**
+ * @author S.V.S.SUDHEEP RAO
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
